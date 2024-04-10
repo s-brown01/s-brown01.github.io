fbf32f320c9a606f573f240a9f42d85cf5aff4bf
s-brown01 <sbrown01@carroll.edu>
2024-04-09 23:13:45 -0600

    stuff again

--- assignments/menu/js/menu2.js
+++ assignments/menu/js/menu2.js
@@ -1,11 +1,19 @@
 function openNav() {
-    document.getElementById("mySidepanel").style.width = "14rem";
-    document.getElementById("mySidepanel").style.padding = "2rem";
+    // 100vw = 100% of the viewport width - Google
+    if (screen.width < 768) {
+        document.getElementById("sideMenu").style.width = "100vw";
+        document.getElementById("sideMenu").style.padding = "1rem";
+
+    }
+    else {
+        document.getElementById("sideMenu").style.width = "33vw";
+        document.getElementById("sideMenu").style.padding = "2rem";
+    }
 
 }
 
 function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
-    document.getElementById("mySidepanel").style.padding = "0";
+    document.getElementById("sideMenu").style.width = "0";
+    document.getElementById("sideMenu").style.padding = "0";
 
 }
