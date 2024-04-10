0874a4ca3969e83984fc437dd9791de32cb6cd18
s-brown01 <sbrown01@carroll.edu>
2024-04-10 00:33:50 -0600

    finished mobile and desktop looks, added figma img

--- assignments/menu/js/menu2.js
+++ assignments/menu/js/menu2.js
@@ -1,12 +1,15 @@
 function openNav() {
     // 100vw = 100% of the viewport width - Google
-    if (screen.width < 768) {
+    // WS school was window.innerWindow is how wide the actual website is
+    // 600 because this only determines how large the sideMenu is: looks weird going to 100vw with 600+ pixels
+    if (window.innerWidth < 600) {
         document.getElementById("sideMenu").style.width = "100vw";
         document.getElementById("sideMenu").style.padding = "1rem";
 
     }
     else {
-        document.getElementById("sideMenu").style.width = "33vw";
+        // if the screen is above 600, have it go 1/4 of the screen
+        document.getElementById("sideMenu").style.width = "25vw";
         document.getElementById("sideMenu").style.padding = "2rem";
     }
 
